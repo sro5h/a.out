23b38dfefc22a0e567e1aeaa555c3534890dffbb
Paul Meffle <meffle.paul@me.com>
2021-09-23 12:25:04 +0200

    Use integer types from stdint.h

--- common/types.h
+++ common/types.h
@@ -3,24 +3,10 @@
 
 #include <stdint.h>
 
-typedef int8_t   aout_s8;
-typedef uint8_t  aout_u8;
-typedef int16_t  aout_s16;
-typedef uint16_t aout_u16;
-typedef int32_t  aout_s32;
-typedef uint32_t aout_u32;
-typedef int64_t  aout_s64;
-typedef uint64_t aout_u64;
+typedef float  float32_t;
+typedef double float64_t;
 
-typedef float    aout_f32;
-typedef double   aout_f64;
-
-static_assert(sizeof(aout_f32) == 4, "aout_f32 must be 4 bytes");
-static_assert(sizeof(aout_f64) == 8, "aout_f64 must be 8 bytes");
-
-typedef ssize_t  aout_ssize;
-typedef size_t   aout_usize;
-typedef int8_t   aout_sbyte;
-typedef uint8_t  aout_ubyte;
+static_assert(sizeof(float32_t) == 4, "float32_t must be 4 bytes");
+static_assert(sizeof(float64_t) == 8, "float64_t must be 8 bytes");
 
 #endif
