7999a6697e77a22f188713af6e31123853f5397d
Paul Meffle <meffle.paul@me.com>
2021-09-28 22:14:05 +0200

    Change output format of `aout_print_bytes`

--- common/util.c
+++ common/util.c
@@ -5,6 +5,7 @@ void aout_print_bytes(
                 uint8_t* bytes, size_t size) {
         assert(bytes);
 
+        printf("0x");
         for (size_t i = 0; i < size; ++i) {
                 printf("%02x", bytes[i]);
         }
